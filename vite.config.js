88a56b74f35a14b25303071ffd0c4ffb3cd93a01
Thiago Menezes <thi.menezes14@gmail.com>
2024-08-12 00:17:36 -0300

    chore: update package-json

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: '/',
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
       src: `${__dirname}/src`,
     }
-  },
-  // compilerOptions: {
-  //   baseUrl: `./`,
-  //   paths: {
-  //     "src/*": [
-  //         "./src/*"
-  //     ]
-  //   }
-  // }
+  }
 })
